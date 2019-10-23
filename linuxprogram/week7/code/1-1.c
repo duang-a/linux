b532099eb77ef230e27e3735989ea213b96de980
wu <2394755178@qq.com>
2019-10-23 17:11:12 +0800

    week7 second

--- linuxprogram/week7/code/1-1.c
+++ linuxprogram/week7/code/1-1.c
@@ -10,7 +10,7 @@ int main()
 	pid_t id;
 	if(write(STDOUT_FILENO, buf, sizeof(buf) - 1) != sizeof(buf) - 1)
 		perror("can't write");
-	printf("ABCDEF");
+	printf("ABCDEF\n");
 	id = fork();
 	if(id < 0)
 		perror("fork error");
